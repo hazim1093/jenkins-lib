91b07e3bfa75875726f4a1772d73c9a69d80c6b9
Hazim <hazim_malik@hotmail.com>
2019-01-07 15:16:57 +0100

    Add code quality analysis step

--- vars/serviceBuildPipeline.groovy
+++ vars/serviceBuildPipeline.groovy
@@ -104,7 +104,26 @@ def call(body) {
                                 }
                             }
                         }
+                        
+                        gitlabCommitStatus(name: "Quality Analysis") {
+                            stage("build & SonarQube analysis") {
+                                node {
+                                    withSonarQubeEnv('My SonarQube Server') {
+                                       sh "${scannerHome}/bin/sonar-scanner"
+                                    }
+                                }
+                            }
 
+                            stage("Quality Gate"){
+                                timeout(time: 1, unit: 'HOURS') {
+                                    def qg = waitForQualityGate()
+                                    if (qg.status != 'OK') {
+                                        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                                    }
+                                }
+                            }
+                        }
+                        
                         gitlabCommitStatus(name: "System test") {
                             systemtestStage([microservice: [name: project, version: buildVersion]], isMergeRequestBuild)
                         }
