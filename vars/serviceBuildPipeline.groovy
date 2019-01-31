1213c3aad0ba62f05bf5c7eaca852954466d8bc0
Hazim Malik <hazim_malik@hotmail.com>
2019-01-31 17:03:33 +0100

    Option to set projectName for multimodule projects

--- vars/serviceBuildPipeline.groovy
+++ vars/serviceBuildPipeline.groovy
@@ -83,7 +83,9 @@ def call(body) {
                                     stage("checkout") {
                                         scmVars = checkout([$class: 'GitSCM', branches: [[name: branchName]], userRemoteConfigs: scm.getUserRemoteConfigs()])
                                         def pom = readMavenPom file: 'pom.xml'
-                                        project = pom.artifactId
+                                        // Explicitly set project name fro property
+                                        // NOTE: Should only be used for multi-module projects
+                                        project = config.projectName ?: pom.artifactId
                                         buildVersion = getVersion()
                                         currentBuild.displayName = "${buildVersion}"
                                     }
